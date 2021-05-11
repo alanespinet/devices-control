3d01cfa9e5cafeaa3d37f8fdb3f8afb7a4e0e9bf
Alan Espinet <aelluvetaal@gmail.com>
2021-05-11 09:58:37 -0400

    Add routing to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import DeviceList from "./components/lists/device-list";
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import DeviceList from './components/lists/device-list';
+import AddDevice from './components/actions/add-device';
 
 function App() {
   return (
     <div className="App">
-      <DeviceList />
+      <BrowserRouter>
+        <>
+          <Route path="/" exact={true} component={DeviceList} />
+          <Route path="/add" component={AddDevice} />
+        </>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/actions/add-device.js
@@ -0,0 +1,7 @@
+const AddDevice = props => {
+    return (
+        <h1>Add Device</h1>
+    );
+}
+
+export default AddDevice;
